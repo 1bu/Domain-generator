ccdde26b7df9373d74d825cc7ec3149789dfc761
Iván Carpio <ivancarpio50@gmail.com>
2022-09-06 23:27:25 +0000

    Mi primer commit

--- src/app.js
+++ src/app.js
@@ -2,10 +2,27 @@
 import "bootstrap";
 import "./style.css";
 
-import "./assets/img/rigo-baby.jpg";
-import "./assets/img/4geeks.ico";
+var pronoun = ["the", "our"];
+var adj = ["great", "big"];
+var noun = ["jogger", "racoon", "suave", "sabroso", "palabres"];
+var extensions = ["es", "eu", "co", "ve"];
 
-window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
-};
+for (let i = 0; i < pronoun.length; i++) {
+  for (let j = 0; j < adj.length; j++) {
+    for (let k = 0; k < noun.length; k++) {
+      for (let l = 0; l < extensions.length; l++) {
+        if (noun[k].endsWith(extensions[l])) {
+          console.log(
+            pronoun[i] +
+              adj[j] +
+              noun[k].slice(0, -1 * extensions[l].length) +
+              "." +
+              extensions[l]
+          );
+        } else {
+          console.log(pronoun[i] + adj[j] + noun[k] + ".com");
+        }
+      }
+    }
+  }
+}
